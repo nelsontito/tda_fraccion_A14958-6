90d33a51037e393e92b6a151efca14b0ea5dd7d6
Nelson Edwin Tito Gironda <nelsontitexx@gamail.com>
2024-09-07 09:21:34 -0400

    1er. Parcial

---
+++ fraccion.java
@@ -0,0 +1,42 @@
+public class fraccion implements Ifraccion{
+
+    public static int main(String[] args) {
+        int num=0;
+        int den=0;
+        private int suma;
+        private int resta;
+        private int multiplicar;
+        private int dividir;
+
+
+        
+
+        public static void sumar(int num, int den){
+
+            int sumar = ((num/den) + (num/den));
+            return.suma;
+        }
+    
+        public static void restar(int num, int den){
+    
+            int restar = ((num/den) - (num/den));
+            return.resta;
+        }
+    
+        public static void multiplicar(int num, int den){
+    
+            int multiplicar = ((num/den) * (num/den));
+            return.multiplicar;
+        }
+    
+        public static void dividir(int num, int den){
+    
+            int dividir = ((num/den) * (den/num));
+            return.dividir;
+        }
+    
+    
+        
+        }
+    } 
+}
